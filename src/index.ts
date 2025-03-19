30cfcdeaff736d261ed6ed0e66f622ee3678266c
Dawid D <57358089+aver1cal@users.noreply.github.com>
2025-03-19 10:42:58 +0100

    fix: remove cherry-pick flags (#2)

--- src/index.ts
+++ src/index.ts
@@ -138,13 +138,7 @@ async function cherryPickExecution(
 
     // Cherry pick
     core.startGroup('Cherry picking')
-    const result = await gitExecution([
-      'cherry-pick',
-      '-m',
-      '1',
-      '--strategy=recursive',
-      `${githubSha}`
-    ])
+    const result = await gitExecution(['cherry-pick', `${githubSha}`])
     if (result.exitCode !== 0 && !result.stderr.includes(CHERRYPICK_EMPTY)) {
       throw new Error(`Unexpected error: ${result.stderr}`)
     }
